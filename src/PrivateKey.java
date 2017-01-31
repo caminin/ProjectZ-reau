2f9e4817a6697cc9b7848a575c2bc62da874d586
alefol <lefol.alexandre@gmail.com>
2017-01-31 14:46:38 +0100

    correction de la fonction decryption

--- src/PrivateKey.java
+++ src/PrivateKey.java
@@ -12,14 +12,13 @@ public class PrivateKey extends Key {
         this.u = u;
     }
 
-    public String decryption(BigInteger[] message){
-        String out = "";
-        for(int i = 0; i < message.length; i++){
-            Log.debug(String.valueOf(Character.toChars(message[i].pow(u.intValue()).mod(n).intValue())),DEBUG);
-           out += Character.toChars(message[i].pow(u.intValue()).mod(n).intValue()).toString();
+    public String decryption(BigInteger[] message) {
+        String out = new String("");
+        for (int i = 0; i < message.length; i++) {
+            Log.debug(String.valueOf(Character.toChars(message[i].modPow(u, n).intValue())), DEBUG);
+            out += String.valueOf(Character.toChars(message[i].modPow(u, n).intValue()));
         }
+        System.out.println(out);
         return out;
     }
-
-
 }
