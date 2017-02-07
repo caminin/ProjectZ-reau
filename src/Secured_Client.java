cbef584c98aee04b02a46c7d69a930f4df05ed08
caminin <charlot.rodolphe@gmail.com>
2017-02-07 16:11:16 +0100

    Add a way to use multiple client

--- src/Secured_Client.java
+++ src/Secured_Client.java
@@ -23,6 +23,7 @@ import java.util.Iterator;
  * Created by caminin on 31/01/17.
  */
 public class Secured_Client extends Application {
+    private static boolean debug=true;
 
     // chat GUI components
     private Stage mainStage;
@@ -36,28 +37,12 @@ public class Secured_Client extends Application {
     @FXML private Button nameButton;
     @FXML private Button sendButton;
 
-    // number of clients connected to the server
-    private static int nbClients;
-
-    // contains all the clients connected to the server
-    private ArrayList<Echange> clients;
-
-    private ServerSocket server_socket;
-    private Socket client_socket=null;
-
     private String id;
-
-    private String myIp="172.29.20.197";
-
-    // used to send messages to the server
-    private ObjectInputStream in=null;
-
-    // used to receive messages from the server
-    private ObjectOutputStream out=null;
+    private Message message;
 
     public Secured_Client(){
         try {
-            server_socket=new ServerSocket(7031,7031, InetAddress.getByName(myIp));
+
             launch();
         }
         catch (Exception e) {
@@ -65,13 +50,7 @@ public class Secured_Client extends Application {
         }
     }
 
-    public void sendMessage(String message){
-        try {
-            out.writeObject(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
 
     public static void main(String args[]){
         Application.launch(args);
@@ -86,59 +65,22 @@ public class Secured_Client extends Application {
     }
 
     public void launch(){
-        try {
-            InetAddress IP=InetAddress.getLocalHost();
-            System.out.println("IP of my system is := "+IP.getHostAddress());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        Echange echange;
-        SendIp send_ip=new SendIp();
-        send_ip.start();
-        ReceiveIP trade_ip=new ReceiveIP(this);
-        trade_ip.start();
-        try {
-            echange=new Echange(server_socket.accept(),nbClients,this);
-            send_ip.stop();
-            clients.add(echange);
-            echange.start();
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        }
-        nbClients++;
+        message=new Message();
+        message.receive(this);
     }
 
-    public void connect(String IP){
-        try {
-            client_socket=new Socket(IP,7031);
-            out=new ObjectOutputStream(client_socket.getOutputStream());
-            in=new ObjectInputStream(client_socket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void sendMessage(String message){
+        this.message.send(message);
     }
 
-    // send a message from a client to all the others
-    public void broadcast(int idSender,String nameSender,String message){
-        Echange client;
-        for(Iterator<Echange> iterator = clients.iterator(); iterator.hasNext();){
-            client=iterator.next();
-            if(client.isAlive()){
-                if(client.getClientId()!=idSender){
-                    System.out.println("server sent to client "+client.getClientName()+" message : "+message);
-                    client.send(nameSender,message);
-                }
-            }
-            // client is no longer connected
-            else{
-                iterator.remove();
-            }
+    public void handleMessage(String message){
+        msgHistory.appendText(message+"\n");
 
-        }
     }
 
     @Override
     public void start(final Stage mainStage) throws Exception {
+
         mainStage.setTitle("chat room");
         Scene nameScene = new Scene((Parent) FXMLLoader.load(getClass().getResource("enterName.fxml")));
         final Scene chatScene = new Scene((Parent) FXMLLoader.load(getClass().getResource("Chatroom.fxml")));
@@ -177,77 +119,6 @@ public class Secured_Client extends Application {
             }
         });
 
-
-
-        // Thread in charge of messages reception
-        ThreadReceptionMessage reception = new ThreadReceptionMessage(in,msgHistory);
-        reception.start();
-    }
-
-
-    private class ReceiveIP extends Thread{
-        DatagramSocket socket;
-
-        Secured_Client client;
-
-        public ReceiveIP(Secured_Client client) {
-            this.client=client;
-            try {
-                socket = new DatagramSocket(7030, InetAddress.getByName(myIp));
-                socket.setBroadcast(true);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-
-        }
-        public void run(){
-            try {
-                //Keep a socket open to listen to all the UDP trafic that is destined for this port
-                System.out.println(getClass().getName() + ">>>Ready to receive broadcast packets!");
-
-                //Receive a packet
-                byte[] recvBuf = new byte[15000];
-                DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-                socket.receive(packet);
-
-                //Packet received
-                //System.out.println(getClass().getName() + ">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
-                System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
-
-                //See if the packet holds the right command (message)
-                String message = new String(packet.getData()).trim();
-                if (message.contains("IP:")) {
-                    message = message.replace("IP:", "");
-                    client.connect(message);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private class SendIp extends Thread{
-        public void run(){
-            while(true){
-                try {
-                    DatagramSocket c;
-                    c = new DatagramSocket();
-                    c.setBroadcast(true);
-                    //Open a random port to send the package
-                    byte[] sendData = ("IP:"+myIp).getBytes();
-
-                    //Try the 255.255.255.255 first
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 7030);
-                    c.send(sendPacket);
-                    System.out.println(getClass().getName() + ">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
-
-                    this.sleep(2000);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
 
